80ab946d377e7de7c4cf6f1b85c9742e85d86a7f
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-03-19 01:26:05 -0400

    Replace Strapi server with basic HTTP server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
-const strapi = require('@strapi/strapi');
-strapi({ appDir: __dirname })
-  .start()
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hostinger is working on port ' + PORT);
+}).listen(PORT, () => {
+  console.log('Test server running on port ' + PORT);
+});
