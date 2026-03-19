8412d01091ab5e759a4e4d659315612cfe6191e0
Michelle M. Muñoz-Dorna <michelle@m3orange.com>
2026-03-19 01:14:53 -0400

    Enhance Strapi startup with error handling
    Add error handling to Strapi server startup

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 'use strict';
 const strapi = require('@strapi/strapi');
-strapi.createStrapi().start();
+strapi({ appDir: __dirname })
+  .start()
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
